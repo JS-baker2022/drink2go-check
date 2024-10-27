3d5e8d7ab77231ab2b81412e9f837bee003b4e4b
booblik2022 <booblik2022@gmail.com>
2024-10-27 22:29:42 +0300

    все одним большим пушем  Что понадела за выхи

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,30 @@
 /* в этот файл добавляет скрипты*/
+import Swiper from '../vendor/swiper/swiper';
+
+new Swiper('.hero__swiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: false,
+  lazy: true, // включаем отложенную загрузку изображений
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+
+  ,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+
+  ,
+});
+
+const menu = document.querySelector('.header__navigation');
+const menuButton = document.querySelector('.header__navigation-burger');
+
+menuButton.onclick = function() {
+  menu.classList.toggle('header__navigation--is-open');
+  menu.classList.toggle('header__navigation--is-close');
+};
